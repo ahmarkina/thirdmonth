444fca28f5e4c42b0633dcb6463a1f57c514cdbf
anastasia.markina <ahmarkina@gmail.com>
2016-12-11 21:24:57 +0100

    my first real version

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Created by anastasia.markina on 2016-12-10.
+ */
+$(document).ready(function(){
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        /*
+        margin: 10,
+        nav: false,
+        dots: false,
+        loop: true,
+        items:1,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+        */
+
+        items:1,
+        margin:10,
+        nav:false,
+        dots: true
+    });
+});
